13df85f59acd99ccea74d3e01f6726f45e6ee2f7
mi_daniel <dboy_mi@yahoo.com>
2012-03-25 10:03:31 +0300

    first commit [includes README and primulcpp.cpp file]

---
+++ primulcpp.cpp
@@ -0,0 +1,11 @@
+//**************************************************************
+// AUTOR      : Daniel
+// DESCRIERE  : sterge instant
+//**************************************************************
+
+#include<iostream>
+using namespace std;
+int main(int argc, char* argv[]){
+	cout<<"Hello@"<<argv[0]<<endl;
+return 0;
+}
